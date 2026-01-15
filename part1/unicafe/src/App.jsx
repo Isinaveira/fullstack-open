f1e0abf46b654e992b30a8e52a70aa230ee47a3b
isinaveira <isinaveira@gmail.com>
2026-01-15 10:38:10 +0100

    Part1 : Exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,51 +1,68 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 
 const Button = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>
-}
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
 
 const Header = (props) => <h1>{props.text}</h1>;
 
-const Counter = (props) => <p>{props.text} {props.count}</p>
+const Counter = (props) => (
+  <p>
+    {props.text} {props.count}
+  </p>
+);
+
+const Statistics = (props) => {
+  return (
+    <>
+      <Header text="Statistics" />
+      <Counter text="goods" count={props.goodsCount} />
+      <Counter text="neutral" count={props.neutralsCount} />
+      <Counter text="bads" count={props.badsCount} />
+      <Counter text="average" count={props.calculateAvg()} />
+      <Counter text="positive" count={props.calculatePositivePercentage()} />
+    </>
+  );
+};
 
 const App = () => {
   const [goodsCount, setGoodsCount] = useState(0);
   const [neutralsCount, setNeutralCount] = useState(0);
   const [badsCount, setBadsCount] = useState(0);
 
-  const handleGoodClick = () =>  setGoodsCount(goodsCount + 1);
+  const handleGoodClick = () => setGoodsCount(goodsCount + 1);
   const handleNeutralsClick = () => setNeutralCount(neutralsCount + 1);
   const handleBadsClick = () => setBadsCount(badsCount + 1);
 
   const calculateAvg = () => {
     const total = goodsCount + neutralsCount + badsCount;
-    if(total == 0 ) return 0;
+    if (total == 0) return 0;
     return (goodsCount - badsCount) / (goodsCount + neutralsCount + badsCount);
-  }
+  };
 
   const calculatePositivePercentage = () => {
     const total = goodsCount + neutralsCount + badsCount;
-    if(total == 0){
-      return 0 +"%";
+    if (total == 0) {
+      return 0 + "%";
     }
     return (goodsCount / total) * 100 + "%";
-  }
+  };
 
   return (
     <>
-      <Header text="Give feedback"/>
+      <Header text="Give feedback" />
       <Button text="good" onClick={handleGoodClick}></Button>
       <Button text="neutral" onClick={handleNeutralsClick}></Button>
       <Button text="bad" onClick={handleBadsClick}></Button>
-      <Header text="Statistics"/>
-      <Counter text="goods" count={goodsCount}/>
-      <Counter text="neutral" count={neutralsCount}/>
-      <Counter text="bads" count={badsCount}/>
-      <Counter text="average" count={calculateAvg()} />
-      <Counter text="positive" count={calculatePositivePercentage()} />
+      <Statistics 
+        goodsCount = {goodsCount}
+        neutralsCount = {neutralsCount}
+        badsCount = {badsCount}
+        calculateAvg = {calculateAvg}
+        calculatePositivePercentage = {calculatePositivePercentage}
+      />
     </>
-  )
-}
+  );
+};
 
 export default App;
