20b39da4ed260284de83bf4d1bc3bd02a636dee3
isinaveira <isinaveira@gmail.com>
2026-02-05 17:21:03 +0100

    Part 2: Exercise 2.6

---
+++ part2/agenda/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas'}
+  ]);
+
+  const [newName, setNewName] = useState('');
+
+  const onNewName = (event) => {
+    setNewName(event.target.value);
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    setPersons([...persons, {name: newName}]);
+    setNewName("");
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={onNewName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person, index) =>  {
+          return <li key={index}>{person.name}</li>
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App
