a4b4e1a6e33392afae352f932dabc98fa346d1c6
isinaveira <isinaveira@gmail.com>
2026-02-05 17:52:00 +0100

    Part 2 : Exercise 2.8

--- part2/agenda/src/App.jsx
+++ part2/agenda/src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    {
+      name: 'Arto Hellas',
+      phone: '040-1234567'
+    }
   ]);
 
   const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState('');
 
   const onNewName = (event) => {
     setNewName(event.target.value);
+  }
+
+  const onNewPhone = (event) => {
+    setNewPhone(event.target.value);
   }
 
   const checkNewPerson = () => {
@@ -18,8 +26,9 @@ const App = () => {
     event.preventDefault();
     checkNewPerson() 
     ? window.alert(`${newName} is already added to phonebook`) 
-      : setPersons([...persons, {name: newName}]);
+      : setPersons([...persons, {name: newName, phone: newPhone}]);
     setNewName("");
+    setNewPhone("");
   }
 
   return (
@@ -30,13 +39,16 @@ const App = () => {
           name: <input value={newName} onChange={onNewName}/>
         </div>
         <div>
+          number: <input value={newPhone} onChange={onNewPhone}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map((person, index) =>  {
-          return <li key={index}>{person.name}</li>
+          return <li key={index}>{person.name} {person.phone}</li>
         })}
       </div>
     </div>
