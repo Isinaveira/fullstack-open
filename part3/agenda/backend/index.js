f67633aa402690b942b3a3aa3830c68532e4d466
isinaveira <isinaveira@gmail.com>
2026-02-12 20:34:33 +0100

    Part 3 : Exercise 3.3

--- part3/agenda/backend/index.js
+++ part3/agenda/backend/index.js
@@ -1,69 +1,81 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3001;
-const baseUrl= `http://localhost:${PORT}/api`;
+const baseUrl = `http://localhost:${PORT}/api`;
 
 app.use(express.json());
 
-
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
 let endpoints = [
-    {
-        route: "/api/persons",
-        description: "Get all persons in the agenda",
-        url: `${baseUrl}/persons`
-    },
-]
+  {
+    route: "/api/persons",
+    description: "Get all persons in the agenda",
+    url: `${baseUrl}/persons`,
+  },
+];
 
 const parseEndpoints = () => {
-    endpoints.map(e => `${e.route} -- ${e.description} -- ${baseUrl}${e.route}`).join('\n');
-}
-
+  endpoints
+    .map((e) => `${e.route} -- ${e.description} -- ${baseUrl}${e.route}`)
+    .join("\n");
+};
 
 app.get("/", (request, response) => {
-    response.status(200).send("App is running, to see API go to /api");
+  response.status(200).send("App is running, to see API go to /api");
 });
 
 app.get("/info", (request, response) => {
-    response.send(`
+  response.send(`
         <p>Phonebook has info for ${persons.length} people </p>
         <p>${new Date()}</p>
     `);
-})
-
+});
 
 app.get("/api", (request, response) => {
-    response.status(200).send(endpoints);
-})
+  response.status(200).send(endpoints);
+});
 
 app.get("/api/persons", (request, response) => {
-    response.json(persons);
-    response.end();
-})
+  response.json(persons);
+  response.end();
+});
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const person = persons.find((person) => person.id === id);
+
+  if (!person) {
+    return response.status(404).json({
+      error: `No data for id ${id}`,
+    });
+  }
+
+  response.json(person);
+});
 
 app.listen(PORT, () => {
-    console.log(`🚀 App ready in http://localhost:${PORT}`);
-})
+  console.log(`🚀 App ready in http://localhost:${PORT}`);
+});
