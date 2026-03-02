aa856e13d4870eeea2525b0806ddb3ca7caed417
isinaveira <isinaveira@gmail.com>
2026-03-02 18:28:25 +0100

    Part 3: Exercise 3.8

--- part3/agenda/backend/index.js
+++ part3/agenda/backend/index.js
@@ -5,7 +5,15 @@ const PORT = 3001;
 const baseUrl = `http://localhost:${PORT}/api`;
 
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body);
+})
+
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
 
 let persons = [
   {
